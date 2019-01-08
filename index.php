9cf90dce4138dc257447e9cfdcdd25c03c63537d
Omer <omer.lakraa@fnacdarty.com>
2019-01-08 14:59:05 +0100

    Inclusion du script

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
 <head>
 	<title>postInator</title>
+	<script src="jquery.js"></script> 	
 	<script src="script.js"></script> 	
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<textarea class="input--medium" id="input"></textarea>
-	<textarea class="input--medium" id="output"></textarea>
+	<textarea class="input--medium" rows="20" id="input"></textarea>
+	<textarea class="input--medium" rows="20" id="output"></textarea>
 </body>
 </html>
