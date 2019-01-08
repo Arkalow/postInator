d91af3a9a1e9ba3c8707d8d96453c61e75925caf
Omer <omer.lakraa@fnacdarty.com>
2019-01-08 15:31:39 +0100

    Recuperation du JSON post, envoie en parametre et affichage de la reponse sur ouput

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+	<input type="text" id="url"/>
 	<textarea class="input--medium" rows="20" id="input"></textarea>
+	<button id="send">SEND</button>
 	<textarea class="input--medium" rows="20" id="output"></textarea>
 </body>
 </html>
