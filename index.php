4402c724008dc24eda08a8be386323bfe267392e
Omer <omer.lakraa@fnacdarty.com>
2019-01-08 14:53:45 +0100

    Ajout de la structure HTML avec le script

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>postInator</title>
+	<script src="script.js"></script> 	
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<textarea class="input--medium" id="input"></textarea>
+	<textarea class="input--medium" id="output"></textarea>
+</body>
+</html>
